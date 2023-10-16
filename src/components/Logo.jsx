468f59a58171fd0c60bc8ea89a62408b50b04315
avanich28 <arpawan.v@gmail.com>
2023-10-16 21:07:59 +0700

    fix image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,8 @@ export function Logo() {
   return (
     <header className="header">
       <h1 className="logo">
-        <img src="../public/woman.svg" alt="woman" />{" "}
-        <span>CV Application</span> <img src="../public/man.svg" alt="man" />
+        <img src="./woman.svg" alt="woman" /> <span>CV Application</span>{" "}
+        <img src="./man.svg" alt="man" />
       </h1>
     </header>
   );
